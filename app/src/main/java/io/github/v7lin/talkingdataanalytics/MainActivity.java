e949a31e7d663b3c30c805e095a9c1c854a82d02
v7lin <v7lin@qq.com>
2019-02-21 21:45:58 +0800

    android talkingdata analytics 1.0.0

--- app/src/main/java/io/github/v7lin/talkingdataanalytics/MainActivity.java
+++ app/src/main/java/io/github/v7lin/talkingdataanalytics/MainActivity.java
@@ -3,10 +3,12 @@ package io.github.v7lin.talkingdataanalytics;
 import android.app.Activity;
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
+
 public class MainActivity extends Activity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
